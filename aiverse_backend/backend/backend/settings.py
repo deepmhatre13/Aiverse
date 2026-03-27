0021f903ee5c8747b27e93189cb8d5e994659272
deepmhatre13 <mhatredeepam@gmail.com>
2026-03-27 22:44:17 +0530

    fix templates config for admin

--- aiverse_backend/backend/backend/settings.py
+++ aiverse_backend/backend/backend/settings.py
@@ -155,7 +155,21 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 # ======================
 # TIME
 # ======================
